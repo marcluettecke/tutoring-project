fbf5b3934ded4b383c6af61d75af6af09a244d57
Marc <marc.luettecke@explicatis.com>
2021-11-21 19:51:17 +0100

    initial commit with angular setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./views/home/home.component";
+import {LoginComponent} from "./views/login/login.component";
+import {SignupComponent} from "./views/signup/signup.component";
+import {AddQuestionComponent} from "./views/add-question/add-question.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'addQuestion', component: AddQuestionComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/header/header.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { SignupComponent } from './views/signup/signup.component';
+import { AddQuestionComponent } from './views/add-question/add-question.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+            declarations: [
+              AppComponent,
+              HeaderComponent,
+              HomeComponent,
+              LoginComponent,
+              SignupComponent,
+              AddQuestionComponent
+            ],
+            imports: [
+              BrowserModule,
+              AppRoutingModule
+            ],
+            providers: [],
+            bootstrap: [AppComponent]
+          })
+export class AppModule {
+};
