3957b350c072602c64e7de39045bd14b8ecb11b9
Marc <marc.luettecke@explicatis.com>
2021-11-28 11:52:14 +0100

    * fix initial routing to login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'addQuestion', component: AddQuestionComponent},
-  {path: '', component: SignupComponent},
+  {path: '', component: LoginComponent},
 ];
 
 @NgModule({
